ede31222b8504c72fe5acdb4340998f7574f4d50
Franck Bourdonnec <fbourdonnec@chez.com>
2022-01-15 03:10:55 +0100

    Avoid the warning about only one rectangular Pad per footprint when the pad as no number

--- klc-check/rules_footprint/F7_3.py
+++ klc-check/rules_footprint/F7_3.py
@@ -20,14 +20,19 @@ class Rule(KLCRule):
         for pad in self.module.pads:
             num = pad['number']
 
-            # Pin 1!
-            if str(num).upper() in self.names:
-                if not pad['shape'] in self.pad_1_shapes:
-                    pad_1_rectangular = False
+            # Unamed pads are for mechanical support
+            # and can be of any shape, no warning
+            if num != '':
+                # Pin 1!
+                if str(num).upper() in self.names:
+                    if not pad['shape'] in self.pad_1_shapes:
+                        pad_1_rectangular = False
 
-            else:
-                if pad['shape'] in self.pad_1_shapes:
-                    other_pads_rectangular = True
+                else:
+                    if pad['shape'] in self.pad_1_shapes:
+                        other_pads_rectangular = True
+
+
 
         if not pad_1_rectangular and len(self.module.pads) >= 2:
             self.warning("Pad 1 should be rectangular")
