c005d6dbf73cd132aa46017124da3561076c0214
aris-kimi <aris_kimi@hotmail.com>
2022-01-17 01:22:36 +0200

    Check for user input and validate repo

--- tools/count_footprints.py
+++ tools/count_footprints.py
@@ -1,13 +1,7 @@
 #! /usr/bin/python3.9
 # -*- coding: utf-8 -*-
-import fnmatch, os
-#Set the path to the directory that git clone creates.
-fp_lib_path = input("Enter footprint library directory path to search in : ")
-fp_dir_type_ending = '.pretty'
-fp_file_type_ending = '.kicad_mod'
-script_dir = os.path.dirname(__file__) #Absolute directory current script is in
-fp_out_rel_path = r"output/list_fp_libs_branch_"
-fp_out_abs_path = os.path.join(script_dir, fp_out_rel_path)
+import fnmatch, os, sys
+
 try:
     from colorama import init, Fore, Back, Style
 except ImportError:
@@ -24,12 +18,34 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 init() #init() function to enable text coloring
+
+#Set the path to the directory that git clone creates.
+fp_lib_path = input("Enter footprint library directory path to search in : ")
+
+if os.path.isdir(fp_lib_path):
+    print(bcolors.OKGREEN + "Folder path is correct!")
+else:
+    #if there are any errors, print 'fail' for these errors
+    print(bcolors.FAIL + "Directory:", bcolors.WARNING + fp_lib_path, bcolors.FAIL + "hasn't found.")
+    print(bcolors.FAIL + "Wrong source.")
+    exit(0)
+fp_dir_type_ending = '.pretty'
+fp_file_type_ending = '.kicad_mod'
+script_dir = os.path.dirname(__file__) #Absolute directory current script is in
+fp_out_rel_path = r"output/list_fp_libs_branch_"
+fp_out_abs_path = os.path.join(script_dir, fp_out_rel_path)
+
 try:
+    import git
     from git import Repo
 except ImportError:
    print("If missing try: pip install gitpython")
    exit(0)
-fp_search_repo = Repo(fp_lib_path)
+try:
+    fp_search_repo = Repo(fp_lib_path)
+except git.exc.GitError:
+   print(bcolors.FAIL + "Missing repo.")
+   exit(0)
 f = open(fp_out_abs_path + fp_search_repo.active_branch.name + ".txt", 'w+')
 totalFiles = 0
 totalDir = 0
