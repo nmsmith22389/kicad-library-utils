be7c81ff44b1cce1e6b114c08b2d693bb872388a
Dominik Wernberger <dominik2.wernberger@tum.de>
2022-04-15 21:49:36 +0200

    Example usage of class instead of dict plus some refactoring

--- common/lib_table.py
+++ common/lib_table.py
@@ -1,20 +1,30 @@
 """
 Library for parsing a library table file (e.g. used for project-specific library settings files).
 """
-import re
-from typing import Dict, List
 
+import re
+from typing import List
+
+
+class LibTableEntry:
+    def __init__(self, name: str, type: str, uri: str, opt: str, desc: str):
+        self.name: str = name
+        self.type: str = type
+        self.uri: str = uri
+        self.opt: str = opt
+        self.desc: str = desc
 
 class LibTable:
+
     def __init__(self, filename: str):
 
-        RE_NAME = r'\(name "?([^\)"]*)"?\)'
-        RE_TYPE = r'\(type "?([^\)"]*)"?\)'
-        RE_URI = r'\(uri "?([^\)"]*)"?\)'
-        RE_OPT = r'\(options "?([^\)"]*)"?\)'
-        RE_DESC = r'\(descr "?([^\)"]*)"?'
+        RE_NAME: str = r'\(name "?([^\)"]*)"?\)'
+        RE_TYPE: str = r'\(type "?([^\)"]*)"?\)'
+        RE_URI: str = r'\(uri "?([^\)"]*)"?\)'
+        RE_OPT: str = r'\(options "?([^\)"]*)"?\)'
+        RE_DESC: str = r'\(descr "?([^\)"]*)"?'
 
-        self.entries: List[Dict[str, str]] = []
+        self.entries: List[LibTableEntry] = []
         self.errors: List[str] = []
 
         with open(filename, "r") as lib_table_file:
@@ -22,7 +32,7 @@ class LibTable:
             for line in lib_table_file:
 
                 # Skip lines that do not define a library
-                if not "(lib " in line:
+                if "(lib " not in line:
                     continue
 
                 re_name = re.search(RE_NAME, line)
@@ -32,14 +42,13 @@ class LibTable:
                 re_desc = re.search(RE_DESC, line)
 
                 if re_name and re_type and re_uri and re_opt and re_desc:
-                    entry = {}
-                    entry["name"] = re_name.groups()[0]
-                    entry["type"] = re_type.groups()[0]
-                    entry["uri"] = re_uri.groups()[0]
-                    entry["opt"] = re_opt.groups()[0]
-                    entry["desc"] = re_desc.groups()[0]
+                    name = re_name.groups()[0]
+                    type = re_type.groups()[0]
+                    uri = re_uri.groups()[0]
+                    opt = re_opt.groups()[0]
+                    desc = re_desc.groups()[0]
+                    entry = LibTableEntry(name=name, type=type, uri=uri, opt=opt, desc=desc)
 
                     self.entries.append(entry)
-
                 else:
                     self.errors.append(line)
--- klc-check/check_lib_table.py
+++ klc-check/check_lib_table.py
@@ -14,6 +14,8 @@ It is important that the official libraries match the entries in these tables.
 import argparse
 import os
 import sys
+from pathlib import Path
+from typing import List
 
 common = os.path.abspath(os.path.join(sys.path[0], "..", "common"))
 if not common in sys.path:
@@ -25,70 +27,65 @@ parser = argparse.ArgumentParser(
     description="Compare a sym-lib-table file against a list of .lib library files"
 )
 parser.add_argument("libs", nargs="+", help=".lib files")
-parser.add_argument("-t", "--table", help="sym-lib-table file", action="store")
+parser.add_argument("-t", "--table", required=True, help="sym-lib-table file", action="store")
 
 args = parser.parse_args()
 
 
-def check_entries(lib_table, lib_names):
+def check_entries(lib_table: LibTable, lib_names: List[str]) -> int:
 
     errors = 0
 
     # Check for entries that are incorrectly formatted
     for entry in lib_table.entries:
-        nickname = entry["name"]
-        uri = entry["uri"]
 
-        if "\\" in uri:
+        if "\\" in entry.uri:
             print(
-                "Found '\\' character in entry '{nick}' - Path separators must be '/'".format(
-                    nick=nickname
-                )
+                f"Found '\\' character in entry '{entry.name}' - Path separators must be '/'"
             )
             errors += 1
 
-        uri_last = ".".join(uri.split("/")[-1].split(".")[:-1])
+        uri_last = Path(entry.uri).stem
 
-        if not uri_last == nickname:
-            print("Nickname '{n}' does not match path '{p}'".format(n=nickname, p=uri))
+        if not uri_last == entry.name:
+            print(f"Nickname '{entry.name}' does not match path '{entry.uri}'")
             errors += 1
 
-    lib_table_names = [entry["name"] for entry in lib_table.entries]
+    lib_table_names = [entry.name for entry in lib_table.entries]
 
     # Check for libraries that are in the lib_table but should not be
     for name in lib_table_names:
-        if not name in lib_names:
+        if name not in lib_names:
             errors += 1
-            print("- Extra library '{l}' found in library table".format(l=name))
+            print(f"- Extra library '{name}' found in library table")
 
         if lib_table_names.count(name) > 1:
             errors += 1
-            print("- Library '{l}' is duplicated in table".format(l=name))
+            print(f"- Library '{name}' is duplicated in table")
 
     # Check for libraries that are not in the lib_table but should be
     for name in lib_names:
-        if not name in lib_table_names:
+        if name not in lib_table_names:
             errors += 1
-            print("- Library '{l}' missing from library table".format(l=name))
+            print(f"- Library '{name}' missing from library table")
 
     # Incorrect lines in the library table
     for error in lib_table.errors:
         errors += 1
         print("- Incorrect line found in library table:")
-        print("  - '{line}'".format(line=error))
+        print(f"  - '{error}'")
 
     return errors
 
 
-lib_names = []
+lib_names: List[str] = []
 
 for lib in args.libs:
-    lib_name = ".".join(os.path.basename(lib).split(".")[:-1])
+    lib_name = Path(lib).stem
     lib_names.append(lib_name)
 
-print("Checking library table - '{table}'".format(table=os.path.basename(args.table)))
-
-print("Found {n} libraries".format(n=len(lib_names)))
+print(f"Checking library table - '{os.path.basename(args.table)}'")
+print(f"Found {len(lib_names)} libraries")
 
 table = LibTable(args.table)
 
