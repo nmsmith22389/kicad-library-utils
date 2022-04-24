ca8a923b7f33bb687444e739492807b20f94c2de
aris-kimi <aris_kimi@hotmail.com>
2022-04-24 23:57:34 +0300

    Added more proper args
    -vvv and -vvvv are not working as expected.
    
    I need to think this better. Seeking for help...

--- tools/count_footprints.py
+++ tools/count_footprints.py
@@ -3,12 +3,32 @@
 import os
 import sys
 import platform
+import argparse
+
+parser = argparse.ArgumentParser(description="Count number of *.kicad_mod files.")
+parser.add_argument(
+    "-d",
+    "--footprint-directory",
+    help="Path to the directory.",
+    action="store",
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    help=(
+        "Screen verbose. -v Shows basic results, -vv Saves report to a file under"
+        " output/ directory, ( Currently NOT WORKING: -vvv Shows extra more, -vvvv"
+        " Saves extra more )"
+    ),
+    action="count",
+)
+args = parser.parse_args()
 
 # Windoze needs special treatment for paths and dirs
 special_host_platform_name = "Windows"
 
-if len(sys.argv) > 1:
-    fp_lib_path = str(sys.argv[1])
+if args.footprint_directory:
+    fp_lib_path = args.footprint_directory
 else:
     # Set the path to the directory that git clone creates.
     fp_lib_path = input("Enter fp library directory path to search in: ")
@@ -20,19 +40,29 @@ common = os.path.abspath(os.path.join(script_dir, os.path.pardir, "common"))
 if common not in sys.path:
     sys.path.append(common)
 
-from print_color import PrintColor
+if args.verbose:
+    verbose_level = int(args.verbose)
+else:
+    verbose_level = 0
 
-printer = PrintColor()
+if verbose_level >= 1:
+    from print_color import PrintColor
+
+    printer = PrintColor()
 
 if os.path.isdir(fp_lib_path):
-    printer.green("Folder path is correct!")
+    if verbose_level >= 1:
+        printer.green("Folder path is correct!")
 else:
     # if there are any errors, print 'fail' for these errors
-    printer.red("Directory:", None, None, True)
-    printer.yellow(fp_lib_path, None, None, True)
-    printer.red("hasn't found.")
-    printer.red("Wrong source.")
-    exit()
+    if verbose_level >= 1:
+        printer.red("Directory:", None, None, True)
+        printer.yellow(fp_lib_path, None, None, True)
+        printer.red("hasn't found.")
+        printer.red("Wrong or missing source directory.")
+    else:
+        print("Wrong or missing source directory.")
+    exit(1)
 
 fp_dir_type_ending = ".pretty"
 fp_file_type_ending = ".kicad_mod"
@@ -50,31 +80,35 @@ try:
     import git
     from git import Repo
 except ImportError:
-    printer.yellow("If missing try: pip install gitpython")
-    exit()
+    if verbose_level >= 1:
+        printer.yellow("If missing try: pip install gitpython")
+    exit(1)
 
 try:
     fp_search_repo = Repo(fp_lib_path)
     found_repo = True
 except git.exc.GitError:
-    printer.yellow("Missing repo.")
+    if verbose_level >= 1:
+        printer.yellow("Missing repo.")
     found_repo = False
 
 if platform.system() == special_host_platform_name:
-    try:
-        os.mkdir(r"output")
-        printer.yellow("output", None, None, True)
-        printer.green("directory created.")
-    except OSError:
-        printer.yellow("output", None, None, True)
-        printer.cyan("directory exists.")
-        pass
+    if verbose_level == 2 or verbose_level == 4:
+        try:
+            os.mkdir(r"output")
+            printer.yellow("output", None, None, True)
+            printer.green("directory created.")
+        except OSError:
+            printer.yellow("output", None, None, True)
+            printer.cyan("directory exists.")
+            pass
 
 if platform.system() == special_host_platform_name:
-    if found_repo:
-        f = open(fp_out_abs_path + fp_search_repo.active_branch.name + ".txt", "w+")
-    else:
-        f = open(fp_out_abs_path_no_git + folder_name + ".txt", "w+")
+    if verbose_level == 2 or verbose_level == 4:
+        if found_repo:
+            f = open(fp_out_abs_path + fp_search_repo.active_branch.name + ".txt", "w+")
+        else:
+            f = open(fp_out_abs_path_no_git + folder_name + ".txt", "w+")
 
 totalFiles = 0
 totalDir = 0
@@ -89,82 +123,113 @@ for base, dirs, files in os.walk(fp_lib_path):
         for file_inside_pretty in os.listdir(base):
             # check the files which end with specific extension
             if file_inside_pretty.endswith(fp_file_type_ending):
+                # TODO correct verbose argument to enable all hidden parts under pretty directories
                 # print path name of selected files
                 # uncomment next line to verbose all fps inside *.pretty dir.
-
-                # print(os.path.join(r'Footprint: ', file_inside_pretty))
+                # if verbose_level >= 3:
+                #    print(os.path.join(r'Footprint: ', file_inside_pretty))
+                #    if verbose_level >= 4:
+                #        f.write(
+                #            "Library: "
+                #            + str(str_list)
+                #            + " Hosts: "
+                #            + str(fps_in_lib)
+                #            + " footprints."
+                #            + "\n"
+                #        )
                 totalFiles += 1
                 fps_in_lib += 1
         if platform.system() == special_host_platform_name:
-            f.write(
-                "Library: "
-                + str(str_list)
-                + " Hosts: "
-                + str(fps_in_lib)
-                + " footprints."
-                + "\n"
-            )
-        print("\n")
-        printer.green("Library:", None, None, True)
-        printer.yellow(str(str_list))
-        printer.green("Hosts:", None, None, True)
-        printer.yellow(str(fps_in_lib), None, None, True)
-        printer.green("footprints.", None, None, True)
-
+            if verbose_level == 2 or verbose_level == 4:
+                f.write(
+                    "Library: "
+                    + str(str_list)
+                    + " Hosts: "
+                    + str(fps_in_lib)
+                    + " footprints."
+                    + "\n"
+                )
+        if verbose_level >= 1:
+            print(os.linesep)
+            printer.green("Library:", None, None, True)
+            printer.yellow(str(str_list))
+            printer.green("Hosts:", None, None, True)
+            printer.yellow(str(fps_in_lib), None, None, True)
+            printer.green("footprints.", None, None, True)
+            if verbose_level >= 3:
+                print(os.linesep)
     for file_inside_nonpretty in os.listdir(base):
         # check the files which end with specific extension
         if file_inside_nonpretty.endswith(fp_file_type_ending):
-            # print path name of selected files, uncomment next line to verbose ALL footprints.
-            # TODO add verbose argument to enable those hidden prints
-            # print(os.path.join(r'Footprint: ' + file_inside_nonpretty))
+
+            # TODO correct verbose argument to enable all hidden parts under non pretty directories
+            if verbose_level >= 3:
+                print(os.path.join(r"Footprint: " + file_inside_nonpretty))
+                if verbose_level >= 4:
+                    f.write("Footprint: " + str(file_inside_nonpretty) + "\n")
             totalTotalFiles += 1
 
 if platform.system() == special_host_platform_name:
-    if found_repo:
-        f.write("\n" + "Current active branch: " + fp_search_repo.active_branch.name)
-    else:
-        f.write("\n" + "Current directory: " + fp_lib_path)
+    if verbose_level == 2 or verbose_level == 4:
+        if found_repo:
+            f.write(
+                "\n" + "Current active branch: " + fp_search_repo.active_branch.name
+            )
+        else:
+            f.write("\n" + "Current directory: " + fp_lib_path)
 
-    f.write("\n" + "Total Number of footprint libraries: " + str(totalDir))
-    f.write(
-        "\n"
-        + "Number of footprint files under *.pretty/ directories: "
-        + str(totalFiles)
-    )
-    # Obsolete or footprint files in non *.pretty library file
-    f.write(
-        "\n"
-        + "Obsolete footprints under non *.pretty: "
-        + str(totalTotalFiles - totalFiles)
-    )
-    f.write("\n" + "TOTAL number of footprint files: " + str(totalTotalFiles))
-    f.close()
-print(os.linesep)
-printer.cyan("Footprint search directory:", None, None, True)
-printer.yellow(fp_lib_path)
+        f.write("\n" + "Total Number of footprint libraries: " + str(totalDir))
+        f.write(
+            "\n"
+            + "Number of footprint files under *.pretty/ directories: "
+            + str(totalFiles)
+        )
+        # Obsolete or footprint files in non *.pretty library file
+        f.write(
+            "\n"
+            + "Obsolete footprints under non *.pretty: "
+            + str(totalTotalFiles - totalFiles)
+        )
+        f.write("\n" + "TOTAL number of footprint files: " + str(totalTotalFiles))
+        f.close()
+if verbose_level >= 1:
+    print(os.linesep)
+    printer.cyan("Footprint search directory:", None, None, True)
+    printer.yellow(fp_lib_path)
 
 if found_repo:
-    printer.cyan("Current active branch:", None, None, True)
-    printer.yellow(fp_search_repo.active_branch.name)
-    if platform.system() == special_host_platform_name:
-        printer.cyan("Output file directory:", None, None, True)
-        printer.yellow(fp_out_abs_path + fp_search_repo.active_branch.name + ".txt")
+    if verbose_level >= 1:
+        printer.cyan("Current active branch:", None, None, True)
+        printer.yellow(fp_search_repo.active_branch.name)
+        if platform.system() == special_host_platform_name:
+            if verbose_level == 2 or verbose_level == 4:
+                printer.cyan("Output file directory:", None, None, True)
+                printer.yellow(
+                    fp_out_abs_path + fp_search_repo.active_branch.name + ".txt"
+                )
 else:
     if platform.system() == special_host_platform_name:
-        printer.cyan("Output file directory:", None, None, True)
-        printer.yellow(fp_out_abs_path_no_git + folder_name + ".txt")
+        if verbose_level == 2 or verbose_level == 4:
+            printer.cyan("Output file directory:", None, None, True)
+            printer.yellow(fp_out_abs_path_no_git + folder_name + ".txt")
 
     else:
-        printer.yellow("No repo found.")
+        if verbose_level >= 1:
+            printer.yellow("No repo found.")
 
-print("\n", end="")
-printer.cyan("Total Number of footprint libraries:", None, None, True)
-printer.yellow(str(totalDir))
-printer.cyan("Number of footprint files under *.pretty/ directories:", None, None, True)
-printer.yellow(str(totalFiles))
-# Obsolete or footprint files in non *.pretty library file
-printer.yellow("Obsolete footprints under non *.pretty:", None, None, True)
-printer.red(str((totalTotalFiles - totalFiles)))
-printer.cyan("TOTAL number of footprint files:", None, None, True)
-printer.yellow(str(totalTotalFiles))
-exit(0)
+if verbose_level >= 1:
+    print("\n", end="")
+    printer.cyan("Total Number of footprint libraries:", None, None, True)
+    printer.yellow(str(totalDir))
+    printer.cyan(
+        "Number of footprint files under *.pretty/ directories:", None, None, True
+    )
+    printer.yellow(str(totalFiles))
+    # Obsolete or footprint files in non *.pretty library file
+    printer.yellow("Obsolete footprints under non *.pretty:", None, None, True)
+    printer.red(str((totalTotalFiles - totalFiles)))
+    printer.cyan("TOTAL number of footprint files:", None, None, True)
+    printer.yellow(str(totalTotalFiles))
+    exit(0)
+else:
+    exit(totalTotalFiles)
