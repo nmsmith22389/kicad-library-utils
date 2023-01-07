03ed424d728e3e74a02ddff06f1d9ac217f8a3eb
Dash <2391085-dash.peters@users.noreply.gitlab.com>
2023-01-07 15:08:35 -0800

    Add check for pin count in footprint filter when pins are missing

--- klc-check/rules_symbol/S5_2.py
+++ klc-check/rules_symbol/S5_2.py
@@ -64,6 +64,57 @@ class Rule(KLCRule):
                     " escaped with '?' or '*'.".format(fp_filter=fp_filter)
                 )
 
+            #  If not all pins are present in the symbol (e.g. NC-pins)
+            #  the pin-count has to be part of the footprint filter
+            # see if any pins are missing, won't work if all NC pins have higher numbers
+            # than the last connected pin
+            int_pins = []
+            for pin in self.component.pins:
+                try:
+                    int_pins.append(int(pin.number))
+                except ValueError:
+                    pass
+
+            if int_pins:
+                missing_pins = []
+                for i in range(1, max(int_pins) + 1):
+                    if i not in int_pins:
+                        missing_pins.append(i)
+
+                if missing_pins:
+                    filter_pin_re = re.search(
+                        r"^[^*?]*"  # package must not have separators (like Infineon*PG*DSO*85*)
+                        # this allows variations (like PQFN) without including manufaturer-specific
+                        # packages, which may not specify number of pins
+                        r"(SOIC|SOIJ|SIP|DIP|SO|SOT\?\d+|SOT\d+|QFN|DFN|QFP|SOP|TO\?\d+|"
+                        r"VSO|PGA|BGA|LLC|LGA)"
+                        r"(\?(?P<padcount>\d+))?[W-_*?$]+",
+                        fp_filter)
+                    if filter_pin_re:
+                        filter_padcount = filter_pin_re.group("padcount")
+
+                        # no  pincount in filter but pins are missing
+                        if not filter_padcount:
+                            self.warning(
+                                "The symbol has omitted pins, but footprint filter '{fp_filter}' "
+                                "does not include the pad count".format(fp_filter=fp_filter)
+                            )
+                        else:
+                            total_pad_count = int(filter_padcount)
+
+                            exposed_pad_re = re.search(r"[?*](?P<ep_count>\d+)EP[?*]", fp_filter)
+                            if exposed_pad_re:
+                                total_pad_count += int(exposed_pad_re.group("ep_count"))
+
+                            # pincount in filter doesn't agree with number of pins on symbol
+                            if total_pad_count < max(int_pins):
+                                self.warning(
+                                    "The symbol has omitted pins and has at least {int_pins} "
+                                    "pins but footprint filter pad count is {padcount}"
+                                    .format(int_pins=(max(int_pins)),
+                                            padcount=filter_padcount)
+                                )
+
     def check(self) -> bool:
         """
         Proceeds the checking of the rule.
