0f25fc1f5405dba8ee96b2d705777d7b4684f657
Dash <2391085-dash.peters@users.noreply.gitlab.com>
2023-01-07 15:00:13 -0800

    Use backwards-compatible pairwise, add license/source to point_is_inside

--- common/kicad_sym.py
+++ common/kicad_sym.py
@@ -8,7 +8,7 @@ import re
 import sys
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Iterable
 
 import sexpr
 
@@ -634,12 +634,43 @@ class Polyline(KicadSymbolBase):
             """Return true iff q is between p and r (inclusive)."""
             return p <= q <= r or r <= q <= p
 
-        for p1, p2 in itertools.pairwise(self.points):
+        def pairwise(iterable: Iterable[Point]):
+            # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+            a, b = itertools.tee(iterable)
+            next(b, None)
+            return zip(a, b)
+
+        for p1, p2 in pairwise(self.points):
             if is_on(p1, p2, point):
                 return True
         return False
 
     def point_is_inside(self, p: Point, sigma: float = 0) -> bool:
+        """
+        Derived from https://wrfranklin.org/Research/Short_Notes/pnpoly.html
+
+        License to use:
+        Copyright (c) 1970-2003, Wm. Randolph Franklin
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this
+        software and associated documentation files (the "Software"), to deal in the Software
+        without restriction, including without limitation the rights to use, copy, modify, merge,
+        publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+        to whom the Software is furnished to do so, subject to the following conditions:
+
+        1. Redistributions of source code must retain the above copyright notice, this list of
+            conditions and the following disclaimers.
+        2. Redistributions in binary form must reproduce the above copyright notice in the
+            documentation and/or other materials provided with the distribution.
+        3. The name of W. Randolph Franklin may not be used to endorse or promote products derived
+            from this Software without specific prior written permission.
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+        INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+        PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+        FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+        DEALINGS IN THE SOFTWARE.
+        """
 
         points_to_check = [p]
         if sigma != 0:
